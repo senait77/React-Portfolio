2107f42dfe6541bc58e200b1c47a621d79f8cb52
Senait <senutekie77@gmail.com>
2021-06-24 19:32:16 -0700

    about component setup

---
+++ src/components/About/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function About() {
+    return(
+    <section className="title">
+      <h1 class="name">about: Senait Gerezgiher</h1>
+      <hr></hr>
+      
+      <div className="row justify-content-center">
+        <div className="col-10" id="about-section">
+            <img class="mb-5" src="../src/assets/images/77768986.jpg" alt="senait tekie"/>
+        <p>
+            I'm baby enamel pin subway tile 90's green juice iPhone you probably haven't heard of them skateboard plaid chillwave pickled kickstarter mixtape. Fanny pack four dollar toast celiac vape. Poutine jianbing pour-over, pickled next level yr gentrify helvetica gluten-free occupy tattooed 3 wolf moon schlitz locavore wolf. Food truck vice pok pok forage vinyl master cleanse pug chartreuse asymmetrical. IPhone four dollar toast four loko +1, before they sold out pok pok yr cray post-ironic pitchfork tacos schlitz aesthetic literally.
+        </p>
+        <p>
+            Fixie meggings lomo, etsy man bun whatever vice salvia sartorial letterpress. Woke hexagon hoodie coloring book fixie. Messenger bag neutra VHS small batch crucifix taiyaki quinoa hammock mlkshk. Franzen tacos cred edison bulb, tbh four loko whatever cliche butcher forage brooklyn PBR&B messenger bag vexillologist. Blue bottle retro tattooed XOXO keytar lomo.
+        </p>
+        <p>
+            Edison bulb chambray live-edge godard hexagon. Neutra lomo cred trust fund health goth hoodie cronut raw denim selvage. Flexitarian vexillologist art party humblebrag. Cardigan health goth sartorial street art small batch iceland everyday carry neutra truffaut.
+        </p>
+        <p>
+            Church-key before they sold out 8-bit knausgaard forage. Woke hoodie pour-over leggings vinyl etsy fingerstache gluten-free air plant subway tile. Vexillologist master cleanse four dollar toast 90's cronut brunch tumblr fam yr franzen microdosing etsy. Copper mug scenester adaptogen, iPhone tousled freegan chicharrones lo-fi etsy keytar taxidermy.
+        </p>
+        <p>
+            Cloud bread tumblr raclette stumptown kickstarter bespoke bicycle rights butcher meggings edison bulb tacos 8-bit cardigan thundercats pinterest. PBR&B hell of butcher, literally selvage austin flexitarian schlitz letterpress +1 brunch swag 90's poutine XOXO. Prism poutine sriracha farm-to-table 8-bit gochujang fam ugh literally shaman. Copper mug viral tbh, echo park banh mi semiotics synth wayfarers kinfolk beard hexagon irony readymade narwhal woke. Keytar coloring book blog street art. Man braid normcore banjo, food truck messenger bag shabby chic helvetica ethical. Vegan you probably haven't heard of them tumeric gastropub lyft vinyl next level cray hashtag keffiyeh waistcoat vaporware fingerstache glossier.
+      </p>
+      <p>
+            View full <a href="#/resume" class="link">resume</a>
+    </p>
+      </div>  
+      </div>
+    </section> 
+)}
+  
+export default About;
